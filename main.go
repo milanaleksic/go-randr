0081096cfb0977b82beccc2230172a4f1cfded4f
Milan Aleksic <milanaleksic@gmail.com>
2019-10-13 13:09:28 +0200

    feature: activate only laptop

--- main.go
+++ main.go
@@ -15,6 +15,41 @@ const RandrApp = "xrandr"
 func main() {
 	displays := parseDisplays(getRandrOutput())
 	log.Printf("Deduced displays: %+V", displays)
+	hdmi := displays["DP-1-1"]
+	hdmi_office := displays["DP-2-1"]
+	hdmi_direct := displays["HDMI-1"]
+	vga_or_dp := displays["DP-1"]
+	laptop := displays["eDP-1"]
+
+	if hdmi != nil && hdmi.state == Connected {
+		// direct hdmi detected
+		// --output eDP-1 --mode 1920x1080 --pos 1920x0 --output DP-1-1 --mode 1920x1080 --pos 0x0
+	} else if hdmi_direct.state == Connected && hdmi_office != nil && hdmi_office.state == Connected {
+		// 2 HDMI screens detected
+		// although xrandr/arandr both _see_ the monitor DP-2-1 being active, it is not!
+		// Thus, I am turning off that screen and only then do I proceed to activate both monitors
+		// --output DP-2-1 --off
+		// --output DP-2-1 --mode 1920x1080 --pos 1920x0 --output HDMI-1 --mode 1920x1080 --pos 0x0 --output eDP-1 --off
+	} else if hdmi_direct.state == Connected {
+		// direct hdmi detected
+		// --output eDP-1 --mode 1920x1080 --pos 0x0 --output HDMI-1 --mode 1920x1080 --pos 1920x0
+	} else if vga_or_dp.state == Connected {
+		// --output eDP-1 --mode 1920x1080 --pos 0x0 --output DP-1 --mode 2048x1152 --pos 1920x0
+	} else {
+		log.Println("Only laptop!")
+		activate(laptop)
+		// --output eDP-1 --mode 1920x1080 --pos 0x0 --output DP-1 --off --output HDMI-1 --off
+	}
+}
+
+func activate(laptop *display) {
+	cmd := exec.Command(RandrApp, "--output", laptop.name,
+		"--mode", fmt.Sprintf("%dx%d", laptop.modes[0].x, laptop.modes[0].y),
+		"--pos", "0x0")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func getRandrOutput() bytes.Buffer {
